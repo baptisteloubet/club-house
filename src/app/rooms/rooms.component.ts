a4a933e5898071bc78b76baa31892a5f37e0e4cc
Baptiste Loubet <baptiste.loubet@outlook.com>
2022-02-17 12:09:29 +0100

    Rooms OnInit

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireDatabase} from '@angular/fire/compat/database';
+import { Router } from '@angular/router';
+import { Observable, Subject } from "rxjs";
+import { map, take, takeUntil } from 'rxjs/operators';
+
+export interface Iroom{
+  key?: string;
+  name?: string;
+  host?: string;
+}
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
-export class RoomsComponent implements OnInit {
+export class RoomsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public $rooms : Observable<Iroom> | undefined | any;
+  public isRoomCreationShown = false;
+  private user :string | undefined;
+  private roomList = this.db.list<Iroom>('rooms');
+  private $destroy: Subject<boolean> = new Subject<boolean>();
+
+
+  constructor(
+    private auth : AngularFireAuth,
+    private route : Router,
+    private db : AngularFireDatabase
+  ) { }
 
   ngOnInit(): void {
+    this.$rooms = this.roomList.snapshotChanges().pipe(
+      map((action)=>action.map((c)=>{
+        return {
+          ...(c.payload.val() as any),
+          key : c.payload.key
+        };
+      })
+      ),
+      takeUntil(this.$destroy)
+    );
+    this.auth.authState.pipe(take(1)).subscribe({
+      next:(user)=>{
+        this.user = user?.uid;
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  logout(): void {
+    this.auth.signOut().then(() => {
+      this.route.navigate(['profile']);
+    })
   }
 
 }
